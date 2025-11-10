a72d779f87fd1c624be97ae19ab325fb6843e945
kelsipage <kelsi.page@gmail.com>
2025-11-10 21:11:30 +0000

    Deploying to gh-pages from @ kelsipage/kelsipage.github.io@9d4bd17faff6372f6cb25c31bf6ef2f07a0f728d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Exploring the world of Education",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -437,8 +437,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-cyber-funding",
+          title: 'Cyber Funding',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";

cb070380b775f02a4c76557115f55373bdea4c7e
kelsipage <kelsi.page@gmail.com>
2025-11-20 16:57:41 +0000

    Deploying to gh-pages from @ kelsipage/kelsipage.github.io@43e36d877fc129a66cda204e9fb2784aea55aa14 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "Take a look at my past professional work and volunteer experience.",
